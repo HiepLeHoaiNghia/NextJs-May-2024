b1e6e427280a89d4472c517243813febdf537fb9
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2024-05-02 09:25:04 +0700

    feat: Basic image usage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,6 +4,16 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 font-sansita text-9xl font-bold'>
       tiếng Việt
+      <Image src='/images/city.jpg' alt='city' width={1280} height={200} quality={100} priority title='insideImage' />
+      <Image
+        src='https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        alt='outsideImage'
+        width={1280}
+        height={200}
+        quality={0}
+        priority
+        title='outsideImage'
+      />
     </main>
   )
 }
